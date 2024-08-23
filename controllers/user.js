d1df3f32021bbc0072799c347edc75ef411d5a82
MBguimaraes <104425878+MateusBorgesGuimaraes@users.noreply.github.com>
2024-08-23 10:43:52 -0300

    New route ro get all saved news by user

--- controllers/user.js
+++ controllers/user.js
@@ -23,18 +23,36 @@ router.put('/:id/save', userExtractor, async (request, response) => {
   }
 
   if (user.savedNews.includes(request.params.id)) {
-    const removed = user.savedNews.filter((id) => id !== request.params.id);
+    const removed = user.savedNews.filter(
+      (id) => id.toString() !== request.params.id,
+    );
     user.savedNews = removed;
-    logger.info(`news ${request.params.id} removed from saved news`);
+    logger.info(
+      `news ${request.params.id} removed. New saved news: ${user.savedNews}`,
+    );
     await user.save();
-    return response.status(204).json({ message: 'news removed to saved news' });
+    return response.status(201).json({ message: 'news removed to saved news' });
   }
 
   user.savedNews.push(request.params.id);
 
-  logger.info(`news ${request.params.id} added to saved news`);
+  logger.info(
+    `user saved after removing news. Current saved news: ${user.savedNews}`,
+  );
   await user.save();
   response.status(201).json({ message: 'news added to saved news' });
+});
+
+// GET SAVED NEWS
+router.get('/saved', userExtractor, async (request, response) => {
+  const user = request.user;
+  if (!user) {
+    return response.status(401).json({ error: 'token missing or invalid' });
+  }
+  const savedNews = await User.findById(user._id)
+    .populate('savedNews')
+    .select('savedNews');
+  response.status(200).json(savedNews);
 });
 
 module.exports = router;
